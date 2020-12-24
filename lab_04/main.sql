cc90b49b49e9d8378bfc5a21e90336829e1c661a
Pavel Khetagurov <khetagurow1@yandex.ru>
2020-12-24 18:54:29 +0300

    Update lab_04

--- lab_04/main.sql
+++ lab_04/main.sql
@@ -58,4 +58,91 @@ returns table (
 select * from get_subject_info_from_sem(1);
 
 -- 4) procedure
-create or replace procedure
+create or replace procedure addAdministrator(id int, typeA int)
+language plpython3u
+as $$
+request = plpy.prepare("insert into administration(IdAdministration, AdministratorType) values($1, $2);", ["int", "int"])
+plpy.execute(request, [id, typeA])
+$$;
+
+call addAdministrator(100, 1);
+
+select * from administration
+where id = 100;
+
+-- 5) Trigger CLR
+drop table if exists transfers;
+create temp table if not exists transfers
+(
+	id serial not null,
+	studentId int,
+	lastGroupId int,
+	newGroupId int
+);
+
+create or replace function groupChangeCLR()
+returns trigger
+language plpgsql
+as $$
+begin 
+	as $$
+	new = TD['new']
+	old = TD['old']
+	id = new['IdAdministration']
+	oldGroup = old["idstudentgroup"]
+	newGroup = new["idstudentgroup"]
+	if oldGroup != newGroup:
+		request = plpy.prepare("insert into transfers(studentId, lastGroupId, newGroupId) values($1, $2, $3);", ["int", "int", "int"])
+		rv = plpy.execute(request, [id, oldGroup, newGroup])
+	return None
+$$ language plpython3u;
+end;
+$$;
+
+drop trigger if exists transfersActionCLR on students;
+create trigger transfersActionCLR
+after update on students
+for each row
+execute function groupChange();
+
+select *
+from studentgroups;
+update students 
+set idstudentgroup = 6685633
+where IdStudent = 65224;
+select * from transfers;
+
+-- 6) Определяемый пользователем тип данных CLR
+drop function if exists get_characteristics();
+drop type if exists characteristics;
+
+create type characteristics as (
+    studentAge int,
+    studentRepositoriesCount int,
+);
+
+create or replace function get_characteristics(id int)
+returns characteristics
+language plpython3u
+as $$
+request = plpy.prepare('''
+select age, cnt
+from (
+	(
+	select age, idstudent
+	from students
+	where id = $1
+	) as qwe join 
+	(
+	select idstudent, count(*) as cnt
+	from students
+	where id = $1
+	group by idstudent
+	) as ewq on qwe.idstudent = ewq.idstudent
+)
+''', ["int"])
+cur_characteristic = plpy.execute(request, [id])
+return (cur_characteristic[0]['age'], cur_characteristic[0]['cnt'])
+$$;
+
+select * from get_characteristics(1);
